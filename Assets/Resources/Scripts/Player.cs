90bae3e68c31ae553c9ddfbc6fafe047d1735d00
Josh Maggard <jmaggard62@gmail.com>
2015-04-18 03:25:32 -0500

    three hit combo

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -24,9 +24,12 @@ public class Player : FAnimatedSprite
     {
         this.tilemap = tilemap;
         addAnimation(new FAnimation(State.IDLE.ToString(), new int[] { 1 }, 100, true));
-        addAnimation(new FAnimation(State.RUN.ToString(), new int[] { 2,3,4,5 }, 100, true));
+        addAnimation(new FAnimation(State.RUN.ToString(), new int[] { 2, 3, 4, 5 }, 100, true));
         addAnimation(new FAnimation(State.SLIDE.ToString(), new int[] { 6 }, 100, true));
         addAnimation(new FAnimation(State.JUMP.ToString(), new int[] { 1, 1, 1, 2 }, 100, true));
+        addAnimation(new FAnimation(State.ATTACK_ONE.ToString(), new int[] { 1, 6 }, 100, false));
+        addAnimation(new FAnimation(State.ATTACK_TWO.ToString(), new int[] { 1, 6 }, 100, false));
+        addAnimation(new FAnimation(State.ATTACK_THREE.ToString(), new int[] { 1, 6 }, 100, false));
         play(State.IDLE.ToString());
 
     }
@@ -94,28 +97,7 @@ public class Player : FAnimatedSprite
                 if (C.getKeyDown(C.ACTION_KEY) && grounded)
                 {
                     StartAttackOne();
-                }
-
-                yVel += gravity * Time.deltaTime;
-                if (xVel > 0)
-                {
-                    xVel = Mathf.Min(maxXVel, xVel);
-                    TryMoveRight();
-                }
-                else if (xVel < 0)
-                {
-                    xVel = Mathf.Max(-maxXVel, xVel);
-                    TryMoveLeft();
-                }
-                if (yVel > 0)
-                {
-                    yVel = Mathf.Min(maxYVel, yVel);
-                    TryMoveUp();
-                }
-                else if (yVel < 0)
-                {
-                    yVel = Mathf.Max(minYVel, yVel);
-                    TryMoveDown();
+                    return;
                 }
 
                 if (grounded && xVel != 0)
@@ -127,13 +109,11 @@ public class Player : FAnimatedSprite
                 }
                 else
                 {
-                        currentState = State.IDLE;
+                    currentState = State.IDLE;
 
                 }
-
-
-
-                xVel *= airFriction;
+                if (grounded)
+                    xVel *= groundFriction;
                 if (Mathf.Abs(xVel) < .1f)
                     xVel = 0;
                 break;
@@ -141,7 +121,20 @@ public class Player : FAnimatedSprite
                 xVel *= groundFriction;
                 if (stateCount > ATTACK_ONE_TIME)
                     currentState = State.IDLE;
-
+                if (C.getKeyDown(C.ACTION_KEY))
+                    StartAttackTwo();
+                break;
+            case State.ATTACK_TWO:
+                xVel *= groundFriction;
+                if (stateCount > ATTACK_TWO_TIME)
+                    currentState = State.IDLE;
+                if (C.getKeyDown(C.ACTION_KEY))
+                    StartAttackThree();
+                break;
+            case State.ATTACK_THREE:
+                xVel *= groundFriction;
+                if (stateCount > ATTACK_THREE_TIME)
+                    currentState = State.IDLE;
                 break;
             case State.SUPERJUMP_CHARGE:
                 break;
@@ -149,22 +142,59 @@ public class Player : FAnimatedSprite
                 break;
         }
 
+        yVel += gravity * Time.deltaTime;
+        if (xVel > 0)
+        {
+            xVel = Mathf.Min(maxXVel, xVel);
+            TryMoveRight();
+        }
+        else if (xVel < 0)
+        {
+            xVel = Mathf.Max(-maxXVel, xVel);
+            TryMoveLeft();
+        }
+        if (yVel > 0)
+        {
+            yVel = Mathf.Min(maxYVel, yVel);
+            TryMoveUp();
+        }
+        else if (yVel < 0)
+        {
+            yVel = Mathf.Max(minYVel, yVel);
+            TryMoveDown();
+        }
+
         stateCount += Time.deltaTime;
 
         this.scaleX = isFacingLeft ? -1 : 1;
-
-        this.play(currentState.ToString());
+            this.play(currentState.ToString());
     }
     private const float ATTACK_ONE_TIME = 1.0f;
     private const float ATTACK_TWO_TIME = 1.0f;
     private const float ATTACK_THREE_TIME = 1.0f;
     private const float ATTACK_ONE_XVEL = 6;
+    private const float ATTACK_TWO_XVEL = 12;
+    private const float ATTACK_THREE_XVEL = 10;
 
     public void StartAttackOne()
     {
         currentState = State.ATTACK_ONE;
         xVel = ATTACK_ONE_XVEL * (isFacingLeft ? -1 : 1);
+        this.play(State.ATTACK_ONE.ToString(), true);
+    }
 
+    public void StartAttackTwo()
+    {
+        currentState = State.ATTACK_TWO;
+        xVel = ATTACK_TWO_XVEL * (isFacingLeft ? -1 : 1);
+        this.play(State.ATTACK_TWO.ToString(), true);
+    }
+
+    public void StartAttackThree()
+    {
+        currentState = State.ATTACK_THREE;
+        xVel = ATTACK_THREE_XVEL * (isFacingLeft ? -1 : 1);
+        this.play(State.ATTACK_THREE.ToString(), true);
     }
 
     private const float SUPERJUMP_CHARGE_TIME = 2.0f;
@@ -213,7 +243,6 @@ public class Player : FAnimatedSprite
             grounded = true;
             if (currentState == State.JUMP)
                 currentState = State.IDLE;
-            xVel *= groundFriction;
             yVel = 0;
             this.y = Mathf.FloorToInt(this.y / tilemap.tileHeight) * tilemap.tileHeight + tilemap.tileWidth / 2;
         }
