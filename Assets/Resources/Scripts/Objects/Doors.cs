26f05f0930c013ce74ed914067febc4fdf153b65
Danves <danves12@gmail.com>
2015-04-19 01:40:23 -0500

    adding Doors class

---
+++ Assets/Resources/Scripts/Objects/Doors.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+	public class Doors : FSprite
+	{
+		private FSprite door;
+		public Doors(Boolean vert)
+		{
+			door = new FSprite("door");
+			isVertical = vert;
+			if (!isVertical)
+			{
+				door.rotation = 90f;
+			}
+			else
+			{
+
+			}
+		}
+
+		private enum States
+		{
+			CLOSED,
+			OPENING,
+			OPEN
+		}
+		private Boolean _isVertical;
+		private Boolean isVertical
+		{
+			get { return _isVertical; }
+			set { _isVertical = value; }
+		}
+
+		public Boolean IsPlayerColliding(Player player)
+		{
+
+			return false;
+		}
+	}
+
